66bdb49f4981122df1690513f47c3dbcb11dc933
Dương Trung Huy <duonghuy.180602@gmail.com>
2025-09-30 10:33:37 +0700

    refactor(api): uncomment and organize GET request handling in bookmark route

--- src/app/api/v1/bookmark/route.ts
+++ src/app/api/v1/bookmark/route.ts
@@ -1,24 +1,24 @@
-// import { handleApiRequest } from "@/lib/uitls/handle-api-request";
-// import { withAuth } from "@/middleware/auth";
-// import { NextRequest, NextResponse } from "next/server";
+import { handleApiRequest } from "@/lib/uitls/handle-api-request";
+import { withAuth } from "@/middleware/auth";
+import { NextRequest, NextResponse } from "next/server";
 
-// export const GET = async (req: NextRequest): Promise<NextResponse> => {
-//     return handleApiRequest(async () => {
-//         const auth = await withAuth([])(req);
-//         if ("error" in auth) {
-//             return NextResponse.json(
-//                 { message: auth.error },
-//                 { status: auth.status }
-//             );
-//         }
+export const GET = async (req: NextRequest): Promise<NextResponse> => {
+  return handleApiRequest(async () => {
+    // const auth = await withAuth([])(req);
+    // if ("error" in auth) {
+    //   return NextResponse.json(
+    //     { message: auth.error },
+    //     { status: auth.status }
+    //   );
+    // }
 
-//         const { searchParams } = new URL(req.url);
-//         const userId = auth.user.id;
-//         const pageParam = searchParams.get("page");
-//         const limitParam = searchParams.get("limit");
-//         const page = pageParam !== null ? Number(pageParam) : undefined;
-//         const limit = limitParam !== null ? Number(limitParam) : undefined;
+    const { searchParams } = new URL(req.url);
+    // const userId = auth.user.id;
+    const pageParam = searchParams.get("page");
+    const limitParam = searchParams.get("limit");
+    const page = pageParam !== null ? Number(pageParam) : undefined;
+    const limit = limitParam !== null ? Number(limitParam) : undefined;
 
-//         return NextResponse.json(bookmarks);
-//     });
-// }
+    return NextResponse.json({ message: "Not implemented" }, { status: 200 });
+  });
+};
